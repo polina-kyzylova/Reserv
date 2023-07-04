a1d9895476d415ac99202d7425853a0c5c5e6f61
Polina Kyzylova <121396943+polina-kyzylova@users.noreply.github.com>
2023-07-04 10:59:54 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.main_menu').click(function(){
+        $('.burger, .main_menu').addClass('open-burger');
+        $('.burger, .main_menu').removeClass('close-burger');
+        $('body').addClass('lock');
+    });
+});
+
+
+$(document).ready(function() {
+    $('.burger_menu').click(function(){
+        $('.burger, .main_menu').addClass('close-burger');
+        $('.burger, .main_menu').removeClass('open-burger');
+        $('body').removeClass('lock');
+    });
+});
